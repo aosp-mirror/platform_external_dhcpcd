ba7d40a700ac90f339011e1465fabe13e57b9d4a
The Android Open Source Project <initial-contribution@android.com>
2009-03-09 11:52:11 -0700

    auto import from //branches/cupcake/...@137197

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ LOCAL_SRC_FILES := common.c dhcp.c dhcpcd.c logger.c net.c \
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS)
 LOCAL_SHARED_LIBRARIES := libc libcutils
 LOCAL_MODULE = dhcpcd
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -20,12 +20,12 @@ LOCAL_SRC_FILES := showlease.c
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS)
 LOCAL_SHARED_LIBRARIES := libc
 LOCAL_MODULE = showlease
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := dhcpcd.conf
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(etc_dir)
 LOCAL_SRC_FILES := android.conf
@@ -33,7 +33,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := dhcpcd-run-hooks
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(etc_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -41,7 +41,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := 01-test
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(hooks_target)
 LOCAL_SRC_FILES := $(hooks_dir)/$(LOCAL_MODULE)
@@ -49,7 +49,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := 20-dns.conf
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(hooks_target)
 LOCAL_SRC_FILES := $(hooks_dir)/$(LOCAL_MODULE)
@@ -57,7 +57,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := 95-configured
-LOCAL_MODULE_TAGS := user development
+LOCAL_MODULE_TAGS := user
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(hooks_target)
 LOCAL_SRC_FILES := $(hooks_dir)/$(LOCAL_MODULE)
