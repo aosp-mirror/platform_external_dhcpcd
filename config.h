09cda4fad9681e8500485ef0b71974d16c28fcc3
Elliott Hughes <enh@google.com>
2014-06-24 14:36:30 -0700

    Move dhcpcd off its own getline.
    Also move other stuff provided by bionic (or unused) out of the way.
    
    Bug: 14499627
    Change-Id: I29e15c5a978d2fa86b63df891c2f2e83eba009fc

--- config.h
+++ config.h
@@ -4,15 +4,6 @@
 #define LIBEXECDIR	"/system/etc/dhcpcd"
 #define DBDIR		"/data/misc/dhcp"
 #define RUNDIR		"/data/misc/dhcp"
-#include "compat/arc4random.h"
 #include "compat/closefrom.h"
-#include "compat/strlcpy.h"
-#include "compat/getline.h"
 
-#ifndef MAX
-#define MAX(a,b)	((a) >= (b) ? (a) : (b))
-#endif
-
-#ifndef MIN
-#define MIN(a,b)	((a) <= (b) ? (a) : (b))
-#endif
+#include <sys/param.h>
